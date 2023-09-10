bdd2defd5d627ff97ad409887d193ad4a94fcc35
SanaAmeer678 <you@example.com>
2023-09-10 06:31:54 -0700

    practice-1

---
+++ Practice_1/index.js
@@ -0,0 +1,74 @@
+// console.log('Hello World');
+
+// console.log('Pakistan Zindabad');
+
+let message = 'Hello World';
+
+// console.log(message);
+
+message = 'I got admission; in Web Development';
+// Alt + Shift + Down (Arrow)
+let message2 = "I got admission; in Web Development";
+
+// console.log(message);
+// console.log(message2);
+
+
+let number1 = 5;
+// console.log(number1);
+let number2 = 15;
+// console.log(number2);
+
+
+const num1 = 5;
+
+// console.log(num1);
+
+// Mathematical Calculation
+
+let a = 26;
+let b = 2;
+
+let add = a + b;
+let sub = a - b + add;  // a - b + (a + b)
+let multi = a * b;
+let div = a / b;
+let modulus1 = a % b;
+let modulus2 = 13 % b;
+let modulus3 = 5 % 3;
+// console.log(add, sub, multi, div);
+
+// console.log(modulus1,
+//   modulus2,
+//   modulus3);
+
+
+// Float Type Numbers
+let floatN = 26.02;
+let floatN2 = 2.343;
+
+let addFloat = floatN + floatN2;
+let subFloat = floatN - floatN2 + addFloat;  // a - b + (a + b)
+let multiFloat = floatN * floatN2;
+let divFloat = floatN / floatN2;
+
+// console.log(addFloat, subFloat, multiFloat, divFloat);
+
+let number_1 = 10;
+let string_1 = 'Hello';
+
+let addNumAndString = string_1 + number_1 + 15;
+let addNumAndString2 = number_1 + 15 + string_1;
+let addNumAndString3 = number_1 + 15 + string_1;
+
+// console.log(addNumAndString);
+// console.log(addNumAndString2);
+// console.log(addNumAndString3);
+
+let name = 'Aiza';
+let age = 19;
+let statement = `My name is ${name} and I am ${age} years old`;
+let statement1 = `${number_1 + 15} ${string_1}`;
+
+console.log(statement);
+console.log(statement1)
